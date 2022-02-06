1b0d28e1f1d2daf56b67809972c2ade7e3cd0d94
SM <s-mcdermott@outlook.com>
2022-02-06 15:03:57 -0800

    .

--- Program.cs
+++ Program.cs
@@ -4,39 +4,47 @@ using Newtonsoft.Json.Linq;
 
 namespace Stepmania2BeatSaber
 {
-    static class Program{
+    static class Program
+    {
         private static readonly string pDir = @"C:\src\BeatSaber\BREAK DOWN!\original";
         private static readonly string pFilename = "BREAK DOWN!.sm";
         private static readonly string pSongName = pFilename.Split(".")[0];
         private static readonly string pChroMapperVersion = "2.2.0";
-        public static void Main(){
+        public static void Main()
+        {
             OrderedDictionary hash = ReadFile();
-            if (hash != null && hash.Keys.Count > 0){
+            if (hash != null && hash.Keys.Count > 0)
+            {
                 var temp = hash["bpm"];
-                if (temp != null){
+                if (temp != null)
+                {
                     Output("BPM Found: " + ((double)temp).ToString(), ConsoleColor.Green);
                 }
                 temp = hash["songs"];
-                 if(temp!= null){
+                if (temp != null)
+                {
                     var songs = ParseSong((OrderedDictionary)temp);
                     WriteFile(songs);
                 }
 
-                
+
             }
             Output("Press any key to exit...");
             Console.ReadKey();
         }
-        public static void WriteFile(OrderedDictionary objectToWrite){
+        public static void WriteFile(OrderedDictionary objectToWrite)
+        {
             string basefilename = "Standard.dat";
             string newPath = pDir + @"\BeatSaber - " + pSongName + @"\";
-            foreach (string key in objectToWrite.Keys){
+            foreach (string key in objectToWrite.Keys)
+            {
                 string filename = newPath + key + basefilename;
                 // ---------
                 JArray notes = new();
                 var temp = objectToWrite[key];
 
-                if (temp != null){
+                if (temp != null)
+                {
                     foreach (Note note in (ArrayList)temp)
                         notes.Add(note.ToJOject());
                     //----------
@@ -45,36 +53,45 @@ namespace Stepmania2BeatSaber
                     if (!Directory.Exists(newPath))
                         Directory.CreateDirectory(newPath);
                     //----------
-                    using (StreamWriter writer = new(filename, false)){
+                    using (StreamWriter writer = new(filename, false))
+                    {
                         writer.Write(chroMapperJSON.ToString());
                     }
                     Output("Wrote File: " + filename, ConsoleColor.Cyan);
                 }
             }
         }
-        public static string GetNextLine(StreamReader sr){
-            try{
+        public static string GetNextLine(StreamReader sr)
+        {
+            try
+            {
                 var retVal = sr.ReadLine();
                 if (retVal == null)
                     return "";
-                return (string) retVal;
+                return (string)retVal;
             }
-            catch{
+            catch
+            {
                 return "";
             }
         }
-        public static OrderedDictionary ReadFile(){
+        public static OrderedDictionary ReadFile()
+        {
             Output("Reading data...", ConsoleColor.Cyan);
             OrderedDictionary playCollection = new();
             OrderedDictionary retHash = new();
             string line;
             string filename = pDir + @"\" + pFilename;
-            try{
-                using (StreamReader reader = new(filename)){
+            try
+            {
+                using (StreamReader reader = new(filename))
+                {
                     // Read one line from file
-                    while (reader != null && !reader.EndOfStream){
+                    while (reader != null && !reader.EndOfStream)
+                    {
                         line = GetNextLine(reader);
-                        if (line != null && line.StartsWith("//") && line.IndexOf("dance-single") > -1){
+                        if (line != null && line.StartsWith("//") && line.IndexOf("dance-single") > -1)
+                        {
                             reader.ReadLine();
                             reader.ReadLine();
                             reader.ReadLine();
@@ -85,19 +102,24 @@ namespace Stepmania2BeatSaber
                             reader.ReadLine();
                             reader.ReadLine();
                             ArrayList notesByDifficulty = new();
-                            while (!reader.EndOfStream){
+                            while (!reader.EndOfStream)
+                            {
                                 var newDifficulty = false;
                                 // Get Noteset
                                 ArrayList noteSet = new();
-                                while (!reader.EndOfStream){
+                                while (!reader.EndOfStream)
+                                {
                                     line = GetNextLine(reader);
-                                    if (line != null){
-                                        if (!line.StartsWith(",") && !line.StartsWith(";")){
+                                    if (line != null)
+                                    {
+                                        if (!line.StartsWith(",") && !line.StartsWith(";"))
+                                        {
                                             noteSet.Add(line);
                                             noteCount += 1;
                                             Output("Note#" + noteCount.ToString("D3") + ":  " + line, ConsoleColor.Yellow);
                                         }
-                                        else{
+                                        else
+                                        {
                                             if (line.StartsWith(";"))
                                                 newDifficulty = true;
                                             break;
@@ -110,7 +132,8 @@ namespace Stepmania2BeatSaber
                             }
                             playCollection.Add(difficulty, notesByDifficulty);
                         }
-                        else if (line != null && line.StartsWith("#DISPLAYBPM:")){
+                        else if (line != null && line.StartsWith("#DISPLAYBPM:"))
+                        {
                             line = line.Replace("#DISPLAYBPM:", "");
                             line = line.Replace(";", "");
                             retHash.Add("bpm", double.Parse(line.Trim()));
@@ -119,73 +142,90 @@ namespace Stepmania2BeatSaber
                 }
                 retHash.Add("songs", playCollection);
             }
-            catch (Exception ex){
+            catch (Exception ex)
+            {
                 Output("Error reading file. " + Environment.NewLine + ex.Message, ConsoleColor.Red);
             }
             return retHash;
         }
-        public static OrderedDictionary ParseSong(OrderedDictionary allData){
+        public static OrderedDictionary ParseSong(OrderedDictionary allData)
+        {
             OrderedDictionary retVal = new();
-            foreach (string key in allData.Keys){
+            foreach (string key in allData.Keys)
+            {
                 Output("Creating song: " + key, ConsoleColor.Cyan);
                 ArrayList notesByDifficulty = new();
                 if (allData != null && allData[key] != null)
-                    try{
+                    try
+                    {
                         var temp = allData[key];
-                        if(temp != null)
+                        if (temp != null)
                             notesByDifficulty = (ArrayList)temp;
                     }
-                    catch{
+                    catch
+                    {
                         notesByDifficulty = new();
                     }
                 ArrayList retArray = new();
                 double baseBeats = 0;
-                if(notesByDifficulty != null){
-                    foreach (ArrayList noteSet in notesByDifficulty){
+                if (notesByDifficulty != null)
+                {
+                    foreach (ArrayList noteSet in notesByDifficulty)
+                    {
                         // Default is 4 notes per measure - we may have more, so calculate the interval
                         double interval = 4.0 / (double)noteSet.Count;
                         if (!interval.Equals(1))
                             Output("Found a different interval: " + interval.ToString(), ConsoleColor.Magenta);
-                        foreach (string noteString in noteSet){
+                        foreach (string noteString in noteSet)
+                        {
                             if (noteString.Length != 4)
                                 throw new NotImplementedException("New Note Count found - expected only 4");
                             // split the noteset into notes
                             int count = 0;
                             char[] noteCharArray = noteString.ToCharArray();
-                            while (count < noteString.Length){
+                            while (count < noteString.Length)
+                            {
                                 char c = noteCharArray[count];
-                                if (!(c.Equals('0'))){
+                                if (!(c.Equals('0')))
+                                {
                                     Note note = new();
                                     if (count > 1)
                                         note.Type = Type.blue;
-                                    switch (count){
-                                        case 0:{
+                                    switch (count)
+                                    {
+                                        case 0:
+                                            {
                                                 note.CutDirection = CutDirection.left;
                                                 note.LineIndex = LineIndex.left;
                                                 break;
                                             }
-                                        case 1:{
+                                        case 1:
+                                            {
                                                 note.CutDirection = CutDirection.down;
                                                 note.LineIndex = LineIndex.centerLeft;
                                                 break;
                                             }
-                                        case 2:{
+                                        case 2:
+                                            {
                                                 note.CutDirection = CutDirection.up;
                                                 note.LineIndex = LineIndex.centerRight;
                                                 break;
                                             }
-                                        case 3:{
+                                        case 3:
+                                            {
                                                 note.CutDirection = CutDirection.right;
                                                 note.LineIndex = LineIndex.right;
                                                 break;
                                             }
-                                        default:{
+                                        default:
+                                            {
                                                 throw new NotImplementedException("Too many notes. Something is critically wrong.");
                                             }
                                     }
                                     note.Time = baseBeats;
                                     retArray.Add(note);
-                                    if (!(c.Equals('1'))){
+                                    if (!(c.Equals('1')))
+                                    {
                                         Output("Found an unexpected note value:" + noteCharArray[count].ToString() + " - Something is wrong.", ConsoleColor.Magenta);
                                     }
                                 }
@@ -195,42 +235,52 @@ namespace Stepmania2BeatSaber
                         }
                     }
                     retVal.Add(key, retArray);
-                }   
+                }
             }
             return retVal;
         }
-        public static string FindDifficulty(string difficulty){
-            switch (difficulty.Split(":")[0].Trim()){
-                case "Beginner":{
+        public static string FindDifficulty(string difficulty)
+        {
+            switch (difficulty.Split(":")[0].Trim())
+            {
+                case "Beginner":
+                    {
                         return "Easy";
                     }
-                case "Easy":{
+                case "Easy":
+                    {
                         return "Normal";
                     }
-                case "Medium":{
+                case "Medium":
+                    {
                         return "Hard";
                     }
-                case "Hard":{
+                case "Hard":
+                    {
                         return "Expert";
                     }
-                default:{
+                default:
+                    {
                         return "";
                     }
             }
         }
-        public static JObject FormatJSON(string version, JArray notes){
+        public static JObject FormatJSON(string version, JArray notes)
+        {
             return new JObject(new JProperty("_version", version),
                 new JProperty("_notes", notes),
                 new JProperty("_obstacles", new JArray()),
                 new JProperty("_events", new JArray()),
                 new JProperty("_waypoints", new JArray()));
         }
-        public static void Output(string outputString, ConsoleColor color){
+        public static void Output(string outputString, ConsoleColor color)
+        {
             Console.ForegroundColor = color;
             Console.WriteLine(outputString);
             Console.ResetColor();
         }
-        public static void Output(string outputString){
+        public static void Output(string outputString)
+        {
             Console.WriteLine(outputString);
         }
     }
